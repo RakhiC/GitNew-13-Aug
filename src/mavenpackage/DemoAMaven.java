a4fe573c2f64170517dadcdd2bf1a5456bff09c1
RakhiC <rakhi.choukimath@gmail.com>
2016-08-13 18:12:01 +0530

    commit

--- src/mavenpackage/DemoAMaven.java
+++ src/mavenpackage/DemoAMaven.java
@@ -4,11 +4,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class DemoAMaven {
 
 	@Test
 	public void testA()
 	{
+		ExtentReports e = new ExtentReports("./extentReport.html");
+		ExtentTest t = e.startTest("testA");
+		t.log(LogStatus.PASS, "Hi-Bye");
+		e.endTest(t);
+		e.flush();
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com/");
 	}
