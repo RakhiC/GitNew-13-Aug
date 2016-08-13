6e177872dca055f9973f865d530b8c493872f5d1
RakhiC <rakhi.choukimath@gmail.com>
2016-08-13 18:25:54 +0530

    test

--- src/mavenpackage/DemoAMaven.java
+++ src/mavenpackage/DemoAMaven.java
@@ -9,16 +9,26 @@ import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
 public class DemoAMaven {
-
+	ExtentReports e = new ExtentReports("./extentReport.html");
 	@Test
 	public void testA()
 	{
-		ExtentReports e = new ExtentReports("./extentReport.html");
+		
 		ExtentTest t = e.startTest("testA");
 		t.log(LogStatus.PASS, "Hi-Bye");
 		e.endTest(t);
-		e.flush();
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com/");
 	}
+	@Test
+	public void testB()
+	{
+		
+		ExtentTest t = e.startTest("testB");
+		t.log(LogStatus.FAIL, "Hi-Bye");
+		e.endTest(t);
+		e.flush();	
+		
+	}
+	
 }
